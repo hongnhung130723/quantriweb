c701f595d7f2efa0ffd52e02e20b725a8372ef91
Vo Thi Hong Nhung <nhung0931063552@gmail.com>
2024-01-18 16:45:39 +0700

    update config

--- config.php
+++ config.php
@@ -1,13 +1,9 @@
 <?php
   const DB_HOST = 'localhost';
-  const DB_USERNAME = 'root';
-  const DB_PASSWORD = "";
-  const DB_NAME = "dbtintonghop";
-  const BASE_DIR = "/webtintonghop/";
+  const DB_USERNAME = 'nhung123';
+  const DB_PASSWORD = "4t,Q3}BP~A.i";
+  const DB_NAME = "dataweb3";
+  const BASE_DIR = "/trangchu/tintuc/";
   const PAGE_SIZE = 10;
   const NUM_RELATED_POST = 3;
   const NUM_POST_HOMEPAGE = 20;
-  
-  
-  
-  
