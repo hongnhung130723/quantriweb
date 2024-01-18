d1651ba21aeb12dd4f5a2ac3e8b8f6d9df245dfd
Vo Thi Hong Nhung <nhung0931063552@gmail.com>
2024-01-18 17:08:54 +0700

    sourcu new

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
 <?php
+  // const DB_HOST = 'localhost';
+  // const DB_USERNAME = 'nhung123';
+  // const DB_PASSWORD = "4t,Q3}BP~A.i";
+  // const DB_NAME = "dataweb3";
+  // const BASE_DIR = "/trangchu/tintuc/";
+  // const PAGE_SIZE = 10;
+  // const NUM_RELATED_POST = 3;
+  // const NUM_POST_HOMEPAGE = 20;
   const DB_HOST = 'localhost';
-  const DB_USERNAME = 'nhung123';
-  const DB_PASSWORD = "4t,Q3}BP~A.i";
+  const DB_USERNAME = 'root';
+  const DB_PASSWORD = "123456";
   const DB_NAME = "dataweb3";
-  const BASE_DIR = "/trangchu/tintuc/";
+  const BASE_DIR = "/Qtws_tinTuc/webtintonghop/";
   const PAGE_SIZE = 10;
   const NUM_RELATED_POST = 3;
   const NUM_POST_HOMEPAGE = 20;
